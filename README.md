bbf64022d6a0f92c44d4b3138c12e4a7d3a5e11e
Nikolay Korolev <korolevns98@gmail.com>
2021-12-31 13:21:20 +0300

    Initial commit with already correct solver

---
+++ README.md
@@ -0,0 +1,13 @@
+# Placement Solver
+Solver for the game [Placement](https://github.com/Chainsawkitten/Placement)  
+Steam Store Page: https://store.steampowered.com/app/700920/Placement
+
+## Usage
+```
+<number of rows> <number of columns>
+<description of field of size rows x columns where 0 - empty, 1 - blocked>
+<string of available pieces (K - King, Q - Queen, N - Knight, R - Rook, B - Bishop, P - Pawn) (can be written in lowercase)>
+```
+
+## Example
+![Example of usage](screenshot.png)
---
+++ main.cpp
@@ -0,0 +1,203 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> calculateAttacks(const vector<vector<char>> &field) {
+    vector<vector<int>> attacks(field.size(), decltype(attacks)::value_type(field[0].size(), 0));
+    for (size_t i = 0; i < field.size(); ++i) {
+        for (size_t j = 0; j < field[i].size(); ++j) {
+            if (field[i][j] == '0' || field[i][j] == '1') continue;
+
+            switch(field[i][j]) {
+            case 'Q':
+                for (size_t k = 1; k <= i; ++k) {
+                    ++attacks[i - k][j];
+                    if (field[i - k][j] != '0') break;
+                }
+                for (size_t k = i + 1; k < field.size(); ++k) {
+                    ++attacks[k][j];
+                    if (field[k][j] != '0') break;
+                }
+
+                for (size_t k = 1; k <= j; ++k) {
+                    ++attacks[i][j - k];
+                    if (field[i][j - k] != '0') break;
+                }
+                for (size_t k = j + 1; k < field[i].size(); ++k) {
+                    ++attacks[i][k];
+                    if (field[i][k] != '0') break;
+                }
+                for (size_t k = 1; k <= min(i, j); ++k) {
+                    ++attacks[i - k][j - k];
+                    if (field[i - k][j - k] != '0') break;
+                }
+                for (size_t k = 1; k <= min(i, field[i].size() - 1 - j); ++k) {
+                    ++attacks[i - k][j + k];
+                    if (field[i - k][j + k] != '0') break;
+                }
+                for (size_t k = 1; k <= min(field.size() - i - 1, field[i].size() - 1 - j); ++k) {
+                    ++attacks[i + k][j + k];
+                    if (field[i + k][j + k] != '0') break;
+                }
+                for (size_t k = 1; k <= min(field.size() - i - 1, j); ++k) {
+                    ++attacks[i + k][j - k];
+                    if (field[i + k][j + k] != '0') break;
+                }
+                break;
+            case 'K':
+                if (i > 0) {
+                    if (j > 0) ++attacks[i - 1][j - 1];
+                    ++attacks[i - 1][j];
+                    if (j + 1 < field[i].size()) ++attacks[i - 1][j + 1];
+                }
+
+                if (j > 0) ++attacks[i][j - 1];
+                if (j + 1 < field[i].size()) ++attacks[i][j + 1];
+
+                if (i + 1 < field.size()) {
+                    if (j > 0) ++attacks[i + 1][j - 1];
+                    ++attacks[i + 1][j];
+                    if (j + 1 < field[i].size()) ++attacks[i + 1][j + 1];
+                }
+
+                break;
+            case 'N':
+                if (i > 1) {
+                    if (j > 0) ++attacks[i - 2][j - 1];
+                    if (j + 1 < field[i].size()) ++attacks[i - 2][j + 1];
+                }
+                if (i > 0) {
+                    if (j > 1) ++attacks[i - 1][j - 2];
+                    if (j + 2 < field[i].size()) ++attacks[i - 1][j + 2];
+                }
+                if (i + 1 < field.size()) {
+                    if (j > 1) ++attacks[i + 1][j - 2];
+                    if (j + 2 < field[i].size()) ++attacks[i + 1][j + 2];
+                }
+                if (i + 2 < field.size()) {
+                    if (j > 0) ++attacks[i + 2][j - 1];
+                    if (j + 1 < field[i].size()) ++attacks[i + 2][j + 1];
+                }
+
+                break;
+            case 'B':
+                for (size_t k = 1; k <= min(i, j); ++k) {
+                    ++attacks[i - k][j - k];
+                    if (field[i - k][j - k] != '0') break;
+                }
+                for (size_t k = 1; k <= min(i, field[i].size() - 1 - j); ++k) {
+                    ++attacks[i - k][j + k];
+                    if (field[i - k][j + k] != '0') break;
+                }
+                for (size_t k = 1; k <= min(field.size() - i - 1, field[i].size() - 1 - j); ++k) {
+                    ++attacks[i + k][j + k];
+                    if (field[i + k][j + k] != '0') break;
+                }
+                for (size_t k = 1; k <= min(field.size() - i - 1, j); ++k) {
+                    ++attacks[i + k][j - k];
+                    if (field[i + k][j + k] != '0') break;
+                }
+                break;
+            case 'R':
+                for (size_t k = 1; k <= i; ++k) {
+                    ++attacks[i - k][j];
+                    if (field[i - k][j] != '0') break;
+                }
+                for (size_t k = i + 1; k < field.size(); ++k) {
+                    ++attacks[k][j];
+                    if (field[k][j] != '0') break;
+                }
+
+                for (size_t k = 1; k <= j; ++k) {
+                    ++attacks[i][j - k];
+                    if (field[i][j - k] != '0') break;
+                }
+                for (size_t k = j + 1; k < field[i].size(); ++k) {
+                    ++attacks[i][k];
+                    if (field[i][k] != '0') break;
+                }
+
+                break;
+            case 'P':
+                if (i > 0) {
+                    if (j > 0) ++attacks[i - 1][j - 1];
+                    if (j + 1 < field[i].size()) ++attacks[i - 1][j + 1];
+                }
+                break;
+
+            default:
+                break;
+            }
+
+        }
+    }
+
+    return attacks;
+}
+
+void solve(vector<vector<char>> &field, string pieces, size_t index = 0) {
+    if (index == pieces.size()) {
+        auto attacks = calculateAttacks(field);
+        for (size_t i = 0; i < field.size(); ++i) {
+            for (size_t j = 0; j < field[i].size(); ++j) {
+                if (field[i][j] != '0' && field[i][j] != '1') {
+                    if (attacks[i][j] != 1) {
+                        return;
+                    }
+                }
+            }
+        }
+
+        for (auto &row : field) {
+            for (auto &value : row) {
+                cout << value;
+            }
+            cout << endl;
+        }
+        cout << endl;
+        return;
+    }
+
+    size_t count = 0;
+    for (size_t i = 0; i < index; ++i) {
+        if (pieces[i] == pieces[index]) {
+            ++count;
+        }
+    }
+
+    for (auto &row : field) {
+        for (auto &value : row) {
+            if (count > 0 && value == pieces[index]) {
+                --count;
+                continue;
+            }
+            if (value != '0' || count > 0) continue;
+            value = pieces[index];
+            solve(field, pieces, index + 1);
+            value = '0';
+        }
+    }
+}
+
+int main()
+{
+    while (true) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<char>> field(n, decltype(field)::value_type(m, {}));
+        for (auto &row : field) {
+            for (auto &value : row) {
+                cin >> value;
+            }
+        }
+        string pieces;
+        cin >> pieces;
+        for (auto &c : pieces) {
+            c = toupper(c);
+        }
+        solve(field, pieces);
+        cout << "Done!" << endl;
+    }
+    return 0;
+}
